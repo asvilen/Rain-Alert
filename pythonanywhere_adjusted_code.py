5b64599dca54afb16e6c4a9d525a0bec155119f7
Svilen Atanasov <a.svilen@gmail.com>
2023-09-03 15:04:14 +0300

    Add files via upload

---
+++ pythonanywhere_adjusted_code.py
@@ -0,0 +1,55 @@
+import requests
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+
+### WEATHER API ###
+###################
+
+api_key = os.environ.get("OWM_API_KEY")
+end_point = "https://api.openweathermap.org/data/2.5/forecast"
+parameters = {
+    'lat'  : 42.653810,
+    'lon'  : 23.314759,
+    'units': 'metric',
+    'appid': api_key
+}
+
+response = requests.get(url=end_point, params=parameters)
+weather_data = response.json()
+
+###  TWILIO API ###
+###################
+def sms_sender():
+    account_sid = "AC72dd09fb5fc1ed43f5eac0e029598255"
+    auth_token = os.environ.get("OWN_AUTH_TOKEN")
+
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+    client = Client(account_sid, auth_token, http_client=proxy_client)
+    message  = client.messages.create(
+                    body="It's going to rain today. Bring an umbrella!",
+                    from_="+19843558644",
+                    to="+359 87 746 1444"
+                )
+    return print(message.status)
+
+### PROGRAM ###
+###############
+
+number_of_forecasts = 4
+will_rain = False
+for hour_index in range(number_of_forecasts):
+    weather_dt = weather_data['list'][hour_index]['dt_txt']
+    weather_id = weather_data['list'][hour_index]['weather'][0]['id']
+    if int(weather_id) < 600:
+        will_rain = True
+        rain_dt = weather_dt
+
+
+if will_rain:
+    sms_sender()
+    print(rain_dt)
+else:
+    print("Will not rain")
